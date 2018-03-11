2fbb736b66457e979385639ccd5e6463100b1f6d
Divyanshu <divyanshu.1996@gmail.com>
2018-03-12 02:05:32 +0530

    taking finger position and sending palmVelocity

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 
 	const fingers = ["THUMB", "INDEX", "MIDDLE", "RING", "PINKY"]
 	var testSocket = new WebSocket("ws://192.168.31.190:8761")
-
+	var data = [];
 	$('#close').on('click', function () {
 		controller.disconnect()
 		testSocket.close()
@@ -40,15 +40,27 @@ $(document).ready(function () {
 			var hand = frame.hands[0]
 			frame.hands[0].fingers.forEach(fingers => {
 				// console.log(fingers.bones)
-				testSocket.send("Fingers position: "+fingers.direction)
+				// testSocket.send("Fingers position: "+fingers.direction)
 			});
 
 			var pitch = hand.pitch()
 			var roll = hand.roll()
-			var yaw = hand.yaw()
-			console.log("pitch: "+pitch+" roll: "+roll+" yaw: "+yaw)
-			console.log("Carp Position: "+ frame.hands[0].fingers[0].carpPosition)
-			console.log("Distal Position: "+ frame.hands[0].fingers[0].dipPosition)		
+			var yaw = hand.yaw() 
+			var indexF = hand.indexFinger
+			var middleF = hand.middleFinger
+			var ringF = hand.ringFinger
+			var pinkyF = hand.pinky;
+
+			data.push(hand.palmVelocity);
+			data.push('$')
+			// data.push(pinkyF.direction)
+			testSocket.send(data)
+			// testSocket.send(hand.palmVelocity)
+			console.log(data)
+			// console.log("pitch: "+pitch+" roll: "+roll+" yaw: "+yaw)
+			// console.log("Carp Position: "+ frame.hands[0].fingers[0].carpPosition)
+			// console.log("Distal Position: "+ frame.hands[0].fingers[0].dipPosition)
+			// console.log("Pinky: "+pinkyF.direction)		
 			// console.log("PalM Velocity: "+frame.hands[0].palmVelocity)
 			
 			var palmNormal = frame.hands[0].palmNormal
