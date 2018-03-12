c6a20c75232260910e6aebc7d134a299b08cce2c
Divyanshu <divyanshu.1996@gmail.com>
2018-03-13 01:20:29 +0530

    phlanx set, angle set, arm set, palm set, f_tip set added

--- pyscripts/server.py
+++ pyscripts/server.py
@@ -15,36 +15,82 @@ from fastdtw import fastdtw
 import glob
 from scipy.spatial.distance import euclidean
 import requests
-
+# from numpy import (array, dot)
+# from numpy.linalg import norm
 
 from sklearn.externals import joblib
 import sklearn.svm
 
+
+
 data = []
 v_set = []
+arm_dir_set = []
+palm_set = []
+f_tip_set = []
+f_angle_set = []
+phlanx_set_pre = []
+phlanx_set = []
+dynamic = False
+static = False
+fuck=0
 async def hello(websocket, path):
+    global dynamic
+    global static
     global data
     global v_set
+    global arm_dir_set
+    global f_tip_set
+    global f_angle_set
+    global phlanx_set_pre
+    global phlanx_set
     count = 0
+    ele = ''
+    elelist = []
     try:
         while True:
             name = await websocket.recv()
-            data = name.split('$')
-            for i in data:
-                count +=1
-                data2 = i.split(',')
-                data2 = list(filter(lambda a: a != '', data2))
-                data2 = [float(i) for i in data2]
-
-                if len(data2):
-                    v_mag = np.linalg.norm(np.array((data2[0],data2[1],data2[2])))
-                    v_set.append(v_mag)
-                    if count%30 == 0:
-                        meanVel =  int(sum(v_set)/len(v_set))
-                        print (meanVel)
-                        v_set = []
+            for i in name:
+                if i != ',':
+                    ele = ele + i
+                else:
+                    elelist.append(ele)
+                    ele = '' 
+            del elelist[-1]
+            elelist = [float(i) for i in elelist]
+            # print (elelist)
+           
+            count +=1
+            arm_dir_set.extend((elelist[9], elelist[10], elelist[11]))
+            palm_set.extend((elelist[3], elelist[4], elelist[5], elelist[6], elelist[7], elelist[8]))
+            f_tip_set.extend((elelist[12], elelist[13], elelist[14], elelist[15], elelist[16], elelist[17], elelist[18], elelist[19], elelist[20], elelist[21], elelist[22], elelist[23], elelist[24], elelist[25], elelist[26]))
+            for i in range(27, 84):
+                phlanx_set_pre.append(elelist[i])  
+            phlanx_set.extend(phlanx_set_pre)        
+            # phlanx_set.extend((elelist[27], elelist[28], elelist[29], elelist[30], elelist[31], elelist[32], elelist[33], elelist[34], elelist[35], elelist[36], elelist[37], elelist[38], elelist[39], elelist[40], elelist[41], elelist[42], elelist[43], elelist[44], elelist[45]))
+            f_angle_set.extend((elelist[84], elelist[85], elelist[86]))
+            v_mag = np.linalg.norm(np.array((elelist[0],elelist[1],elelist[2])))
+            v_set.append(v_mag)
+            if count%30 == 0:
+                
+                meanVel =  int(sum(v_set)/len(v_set))
+                if meanVel < 100:
+                    static = True
+                    dynamic = False
+                else:
+                    dynamic = True
+                    static = False
+                
+                # print (meanVel)
+                v_set = []
+            
+            print (phlanx_set)
+            elelist = []
+            phlanx_set = []
+            f_angle_set = []
                 
     except Exception as ex:
+        print ("Ye fuck kr rha hai: ",fuck)
         print (ex.__class__.__name__)
     finally:
         sys.exit(0)
@@ -52,3 +98,9 @@ start_server = websockets.serve(hello, '192.168.31.190', 8761 )
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+
+def angle_between(v1, v2):
+    c = np.dot(v1,v2)/np.linalg.norm(v1)/np.linalg.norm(v2)
+    return c
+
