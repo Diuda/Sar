b051fa5e0b535be72a91233c12cbb9b9cc9d975f
Divyanshu <divyanshu.1996@gmail.com>
2018-03-13 01:20:57 +0530

    all data sent to socket and Leap vec3 integrated along with magnitue function

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ $(document).ready(function () {
 	const fingers = ["THUMB", "INDEX", "MIDDLE", "RING", "PINKY"]
 	var testSocket = new WebSocket("ws://192.168.31.190:8761")
 	var data = [];
+	var diff_finger_bones = Leap.vec3.create();
+	var crossProd = Leap.vec3.create();
 	$('#close').on('click', function () {
 		controller.disconnect()
 		testSocket.close()
@@ -46,17 +48,78 @@ $(document).ready(function () {
 			var pitch = hand.pitch()
 			var roll = hand.roll()
 			var yaw = hand.yaw() 
-			var indexF = hand.indexFinger
-			var middleF = hand.middleFinger
-			var ringF = hand.ringFinger
-			var pinkyF = hand.pinky;
+			var thumb = hand.fingers[0].tipPosition
+			var indexF = hand.fingers[1].tipPosition
+			var middleF = hand.fingers[2].tipPosition
+			var ringF = hand.fingers[3].tipPosition
+			var pinkyF = hand.fingers[4].tipPosition
+			var palmVelocity = hand.palmVelocity;
+			var palmNormal = hand.palmNormal;
+			var palmPosition = hand.palmPosition;
+			
 
 			data.push(hand.palmVelocity);
-			data.push('$')
-			// data.push(pinkyF.direction)
+			data.push(palmPosition)
+			data.push(palmNormal)
+			data.push(pitch)
+			data.push(roll)
+			data.push(yaw)
+			data.push(thumb)
+			data.push(indexF)
+			data.push(middleF)
+			data.push(ringF)
+			data.push(pinkyF)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[0].pipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[0].dipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[0].tipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[1].mcpPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[1].pipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[1].dipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[1].tipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[2].mcpPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[2].pipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[2].dipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[2].tipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[3].mcpPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[3].pipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[3].dipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[3].tipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[4].mcpPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[4].pipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[4].dipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			Leap.vec3.subtract(diff_finger_bones, hand.fingers[4].tipPosition, hand.palmPosition)
+			data.push(diff_finger_bones)
+			crossProd = Leap.vec3.dot(thumb, indexF)
+			data.push(crossProd/(magnitude(thumb)*magnitude(indexF)))
+			crossProd = Leap.vec3.dot(indexF, ringF)
+			data.push(crossProd/(magnitude(indexF)*magnitude(ringF)))
+			crossProd = Leap.vec3.dot(ringF, pinkyF)
+			data.push(crossProd/(magnitude(ringF)*magnitude(pinkyF)))
+
+			data.push(",")
 			testSocket.send(data)
+			// console.log(data)
+			data = []
 			// testSocket.send(hand.palmVelocity)
-			console.log(data)
+			
 			// console.log("pitch: "+pitch+" roll: "+roll+" yaw: "+yaw)
 			// console.log("Carp Position: "+ frame.hands[0].fingers[0].carpPosition)
 			// console.log("Distal Position: "+ frame.hands[0].fingers[0].dipPosition)
@@ -70,3 +133,12 @@ $(document).ready(function () {
 	})
 
 })
+
+function magnitude(arr){
+	square = 0
+	arr.forEach(ele =>{
+		square = square + (ele*ele);
+	})
+	return Math.sqrt(square)
+
+}
