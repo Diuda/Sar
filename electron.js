5e5d0e3210b50e05d151503cccc82ed789ee4e86
Divyanshu <divyanshu.1996@gmail.com>
2018-03-17 12:33:30 +0530

    electron js file created

---
+++ electron.js
@@ -0,0 +1,19 @@
+const electron = require('electron')
+
+const {app, BrowserWindow} = require('electron')
+  const path = require('path')
+  const url = require('url')
+  
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 1360, height: 720})
+  
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
+  
+  app.on('ready', createWindow)
